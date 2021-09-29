64e3875321ecbb06713bde7bb6f6137774c8ec64
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2021-09-28 23:49:19 -0500

    feat (rutify) configure router

---
+++ src/pages/index.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { metatags, url } from "@roxi/routify";
+
+  metatags.title = "My Routify app";
+  metatags.description = "Description coming soon...";
+</script>
+
+<h1>Hello world</h1>
+
+<a href={$url("./home")}> A link to the introduction section on your left </a>

c4592f4d1bab49a90d405b77e67a7caa782adea2
Eduardo Avila <contact@eduaravila.com>
2023-07-31 22:33:17 -0600

    update(foote) remove links from footer to make it less redundant

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -7,15 +7,8 @@
 
 <footer
   class="footer bottom-0 p-4 h-18	w-full border-b-2 border-green-400 text-primary-dark dark:text-gray-50 text-center my-5"
->
-  <div class="flex gap-2 justify-center flex-col sm:flex-row my-5">
-  {#each items as socialLink}
-    <div >
-      <a href={socialLink.url} target="_blank" rel="noopener noreferrer" class="underline text-primary-dark dark:text-gray-50">
-        {socialLink.urlname}
-      </div>
-  {/each}
-  </div>
+>  
+<a href="/"> Contact info </a>
   <p>©{year} Eduardo Avila.</p>
   <p>made with ❤️</p>
 </footer>
